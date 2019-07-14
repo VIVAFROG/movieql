030cd57001faeb11b249cfd97209706c5ae36a56
VIVAFROG <vivafrog@gmail.com>
2019-07-14 23:09:11 +0900

    #11

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,25 +1,10 @@
 import {
     getMovies,
-    getById,
-    addMovie,
-    deleteMovie
 } from "./db";
 
 const resolvers = {
     Query: {
-        movies: () => getMovies(),
-        movie: (_, {
-            id
-        }) => getById(id)
-    },
-    Mutation: {
-        addMovie: (_, {
-            name,
-            score
-        }) => addMovie(name, score),
-        deleteMovie: (_, {
-            id
-        }) => deleteMovie(id)
+        movies: () => getMovies()
     }
 };
 
